c74b6d3aa8b6caa3b3d6d1bc580517540c0d20e9
shrinivas ghungrad <shrighungrad45@gmail.com>
2023-12-26 21:01:13 +0530

    data storage in localstorage

--- src/App.js
+++ src/App.js
@@ -2,32 +2,27 @@ import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-
-let Dummy_Expense = [
-  
-];
-
 const App = () => {
-  const [expenses,setExpenses]= useState(Dummy_Expense);
-  //array of object
-  fetch('http:localhost/sample-api/api/read.php').then(
-    response=>{
-      return response.json();
+  const [expenses, setExpenses] = useState(() => {
+    const expenseItems = localStorage.getItem("expenses");
+    const parseData = JSON.parse(expenseItems) ?? [];
+    parseData.map((item) => {
+      return (item.date = new Date(item.date));
+    });
+    return parseData;
+  });
 
-    }
-    
-  ).then(
-    data=>{
-      console.log(data);
-      setExpenses(data)
-    }
-  );
-  
-  const addExpenseHandler = (expense) => {
-    const updatedExpense = [expense,...expenses];
+  const addExpenseHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+
+    const updatedExpense = [expenseData, ...expenses];
     setExpenses(updatedExpense);
+    localStorage.setItem("expenses", JSON.stringify(updatedExpense));
   }; //line 35 expense variable recive data from NewExpense which is child and
-    // data is collecte in app.js which is parent and for this process send data from child to parent 
+  // data is collecte in app.js which is parent and for this process send data from child to parent
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -5,22 +5,9 @@ import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
 
-  const saveExpenseDataHandler=(enteredExpenseData)=>{
-  
-    const expenseData={
-      ...enteredExpenseData,id:Math.random().toString()
-    }
-    props.onAddExpense(expenseData);
-
-   console.log(expenseData);
-  };
-
-
-
-
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}  />
+      <ExpenseForm onSaveExpenseData={props.onAddExpense}  />
     </div>
   );
 };
